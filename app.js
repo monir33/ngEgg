3303119e7c30a82ef55221fbb1dab6511b8224d2
monir33 <monir33@gmail.com>
2015-10-03 15:49:31 -0700

    defining method in a scope

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
 angular
-.module('app',['ui.router'])
-.config(function config($stateProvider) {
-	$stateProvider
-	.state('index',{
-		url:'',
-		controller: 'firstCtrl as first',
-		templateUrl: 'templates/first.html'
-	})
-	.state('second',{
-		url: '/second',
-		controller: 'secondCtrl as second',
-		templateUrl: 'templates/second.html'
-	})
-})
-.service('greeting', function () {
-	var greeting = this;
-	greeting.message = 'default';
-})
-.controller('firstCtrl', function( greeting){
-	var first = this;
-	first.greeting = greeting;
-	// $scope.first = this ;
-	// $scope.first = greeting;
-})
-.controller('secondCtrl', function( greeting){
-	var second = this;
-	second.greeting = greeting;
-	// $scope.second = this;
-	// $scope.second = greeting;
-})
+.module('app', [])
+.factory('Data', [function () {
+	
 
+	return {
+		message: 'default message'
+	};
+}])
+.controller('firstCtrl', ['$scope', 'Data', function ($scope,Data) {
+	$scope.data = Data;
+}])
+.controller('secondCtrl', ['$scope', 'Data', function ($scope,Data) {
+	$scope.data = Data;
+	$scope.reverse = function(text){
+		return text.split('').reverse().join('');
+	}
+}])
