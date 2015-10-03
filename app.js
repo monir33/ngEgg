51047f4b14c70d2ea2985aa7f3d248707140cf4c
monir33 <monir33@gmail.com>
2015-10-03 14:05:55 -0700

    the dot

---
+++ app.js
@@ -0,0 +1,12 @@
+angular
+.module('app',[])
+.config([function config($stateProvider) {
+	
+}])
+.controller('firstCtrl', function($scope){
+	
+})
+.controller('secondCtrl', function($scope){
+	
+})
+
